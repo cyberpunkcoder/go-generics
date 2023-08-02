9452d727452cec060c28bf07f377fbfb70bc302b
cyberpunkcoder <cyberpunkcoder@gmail.com>
2023-08-02 10:03:43 -0700

    update example1 comment

--- example1/main.go
+++ example1/main.go
@@ -1,8 +1,9 @@
 package main
 
-/* This example shows how generics can be used to 
-   create a generic function that can be used with
-   different types.
+/*
+This example shows how generics can be used to
+create a generic function that can be used with
+different types.
 */
 
 import (
@@ -11,21 +12,23 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
-// Add adds two values of the same type.
-// The type must implement the Ordered interface.
-// The ordeered interface contains the following types:
-//   - int
-//   - int8
-//   - int16
-//   - int32
-//   - int64
-//   - uint
-//   - uint8
-//   - uint16
-//   - uint32
-//   - uint64
-//   - float32
-//   - float64
+/*
+Add adds two values of the same type.
+The type must implement the Ordered interface.
+The ordered interface contains the following types:
+- int
+- int8
+- int16
+- int32
+- int64
+- uint
+- uint8
+- uint16
+- uint32
+- uint64
+- float32
+- float64
+*/
 func Add[T constraints.Ordered](a, b T) T {
 	return a + b
 }
