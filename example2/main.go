a85b3f52cfd31279e7449ec0f7f48ffccc04eec0
cyberpunkcoder <cyberpunkcoder@gmail.com>
2023-08-02 10:15:27 -0700

    add example2

--- example2/main.go
+++ example2/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+/*
+This example shows that a tilde can be used to
+use any type that is an alias for an underlying type.
+*/
 
+import "fmt"
+
+type UserID int
+
+/*
+ Here we use the tilde to specify that the type
+ can be any type that is an alias for an int.
+ */
+func Add[T ~int](a, b T) T {
+	return a + b
+}
+
+func main() {
+	a := UserID(1)
+	b := UserID(2)
+	result := Add(a,b)
+	fmt.Printf("result: %+v\n", result)
 }
